f157bfbc7cbb5e07da360749d5fd4c6eeecbd490
Saklain Mustak <saklainmustak@sakmac.local>
2022-02-11 04:20:16 +0600

    deposite connected to balance and working with withdraw

--- js/banking.js
+++ js/banking.js
@@ -3,9 +3,30 @@
 document.getElementById('deposit-btn').addEventListener('click',function(){
     //get the amount deposite
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositText = depositInput.value;
+    const newDepositAmount =parseFloat(newDepositText);
+    //console.log(depositAmount);
+    
+    //update deposit total
+
     const depositTotal = document.getElementById('deposit-total');
-    console.log(depositTotal.innerText);
-   depositTotal.innerText = depositAmount;
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmoutn = parseFloat(previousDepositText);
+    const newDepositTotal =previousDepositAmoutn + newDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    //clear input field
+    depositInput.value = '';
+
+});
+
+document.getElementById('withdraw-btn').addEventListener('click',function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    console.log(withdrawInput);
 })
