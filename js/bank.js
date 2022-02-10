fe077e18ec8491fecbae787e9c652389ace0cd87
Saklain Mustak <saklainmustak@sakmac.local>
2022-02-10 11:20:15 +0600

    button navigation to another page

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,17 @@
 document.getElementById('login-submit').addEventListener('click',function(){
     //console.log('button clicked');
+        //get user email
+    const emailField = document.getElementById('user-email');
+   // console.log(emailField);
+   const userEmail = emailField.value;
+   //console.log(userEmail);
+
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    //console.log(userPassword);
+    
+    if(userEmail == 's@gmail.com' && userPassword == 'secret'){
+        location.href= 'banking.html';
+    }
 })
